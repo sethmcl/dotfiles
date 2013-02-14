9e80f685a702e0621f03548ebc4329907086843e
Seth McLaughlin <s@sethmcl.com>
2013-02-13 23:22:45 -0800

    Don't copy readme and install-deps

--- sync.sh
+++ sync.sh
@@ -2,7 +2,7 @@
 cd "$(dirname "$0")"
 git pull
 function doIt() {
-	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "sync.sh" --exclude "README.md" -av . ~
+	rsync --exclude "readme.md" --exclude "install-deps.sh" --exclude ".git/" --exclude ".DS_Store" --exclude "sync.sh" --exclude "README.md" -av . ~
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt
